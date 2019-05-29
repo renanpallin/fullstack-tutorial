557fdbc8c15aad7c5f47f86cd553d1d5bd683c9a
Renan Pallin <renanpallin@gmail.com>
2019-05-29 20:12:29 -0300

    Authorization headers

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,30 +1,38 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
-
-
-import { ApolloProvider } from 'react-apollo';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Pages from './pages';
-
-
+import { ApolloProvider } from "react-apollo";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-    uri: 'http://localhost:4000/'
-})
+// const link = new HttpLink({
+//     uri: "http://localhost:4000/",
+// });
 const client = new ApolloClient({
     cache,
-    link
-})
+    link: new HttpLink({
+        uri: "http://localhost:4000/graphql",
+        headers: {
+            authorization: localStorage.getItem("token"),
+        },
+    }),
+});
+
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem("token"),
+        cartItems: [],
+    },
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
         <Pages />
     </ApolloProvider>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
 
 // client.query({
@@ -39,4 +47,3 @@ ReactDOM.render(
 //         }
 //     `
 // }).then(result => console.log(result))
-
