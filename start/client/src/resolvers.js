9af14fc46d80ad7ed0372850bdd238d416924921
Renan Pallin <renanpallin@gmail.com>
2019-05-29 20:42:36 -0300

    Finish!

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -23,4 +23,16 @@ export const resolvers = {
             return cartItems.includes(launch.id);
         },
     },
+    Mutation: {
+        addOrRemoveFromCart: (_, { id }, { cache }) => {
+            const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+            const data = {
+                cartItems: cartItems.includes(id)
+                    ? cartItems.filter(i => i !== id)
+                    : [...cartItems, id],
+            };
+            cache.writeQuery({ query: GET_CART_ITEMS, data });
+            return data.cartItems;
+        },
+    },
 };
